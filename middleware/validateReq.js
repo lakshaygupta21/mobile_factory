7de41a54af2295fd10912db3878aa204204baab7
Lakshay Gupta <guptalakshay90@gmail.com>
2024-03-05 13:22:48 +0530

    Handled empty body

--- middleware/validateReq.js
+++ middleware/validateReq.js
@@ -2,12 +2,11 @@ const { hasDuplicates } = require("../utils/utils");
 
 function validateReq(req, res, next){
     const components = req.body.components;
-
-    if(hasDuplicates(components)){
-        return res.status(400).json({"error": "Duplicate components found!"})
-    }
     if(!components || components == null){
         return res.status(400).json({"error": "Invalid input!"});
+    }
+    if(hasDuplicates(components)){
+        return res.status(400).json({"error": "Duplicate components found!"})
     }
     if(components.length != 5){
         return res.status(400).json({"error": "Please input all the required components!"});
